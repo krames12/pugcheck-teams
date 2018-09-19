760634d485ede747f4f6e7c40caeea46a969fa5b
Nick Kramer <nkramer@cwims.com>
2018-09-19 16:15:11 -0400

    roster list staying ALL rosters, slightly styled

--- resources/views/rosters/index.blade.php
+++ resources/views/rosters/index.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>All Rosters</h1>
-    <a href="rosters/create">
-        <button class="float-right btn bg-transparent hover:bg-blue rounded border border-blue text-grey-darkest hover:text-white px-2 py-2">
-            New Roster
-        </button>
-    </a>
-    <ul>
-        @foreach($rosters as $roster)
-            <li>
-                <a href="{{ route('rosterShow', $roster->id) }}">{{ $roster->name }}</a>
-            </li>
-        @endforeach
-    </ul>
+    <div>
+        <h1 class="inline-block">All Rosters</h1>
+        <a href="rosters/create" class="float-right">
+            <button class=" btn bg-blue hover:bg-blue-dark rounded border border-blue text-white px-2 py-2">
+                New Roster
+            </button>
+        </a>
+    </div>
+    <div class="row">
+        <div class="mt-4 p-2 col-sm-12 mx-auto">
+            <ul class="list-reset">
+                @foreach($rosters as $roster)
+                    <li class="border-bottom  p-2 mx-4 hover:bg-grey-lighter">
+                        <a href="{{ route('rosterShow', $roster->id) }}">{{ $roster->name }} - {{ $roster->realm->name }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @endsection
